9e8a8cda79c0e7c5279dedb9a4cdb1f04e972ad6
secw01f <secw01f@kali.localdomain>
2020-04-27 12:25:00 -0400

    Updated

--- nsl.sh
+++ nsl.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-ls /usr/share/nmap/scripts/*$1* | awk 'BEGIN { FS = "[/.]" }{print $6}'
+if [ $# == 0 ]
+then
+	echo "nsl"
+	echo ""
+	echo "Usage"
+	echo "./nsl.sh <SearchTerm>"
+	echo "all   use \"all\" (without quotes) to list all scripts"
+	echo ""
+	echo "Example:"
+	echo "./nsl.sh smb"
+	echo "./nsl.sh all"
+fi
+
+if [ $1 == 'all' ]
+then
+	ls /usr/share/nmap/scripts/** | awk 'BEGIN { FS = "[/.]" }{print $6}'
+else
+	while (( "$#" ))
+	do
+		ls /usr/share/nmap/scripts/*$1* | awk 'BEGIN { FS = "[/.]" }{print $6}'
+		shift
+	done
+fi
